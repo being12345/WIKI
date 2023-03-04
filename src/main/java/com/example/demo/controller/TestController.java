7f3794ec8fbe5fb46f4933cea5a4da21eb4f95ae
being <89558990+being12345@users.noreply.github.com>
2023-03-04 21:38:46 +0800

    2-5 customize properties

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,9 +16,13 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    //customize properties if properties isn't set using default: Test
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+        return "Hello!" + testHello;
     }
 
     @PostMapping("/hello/post")
