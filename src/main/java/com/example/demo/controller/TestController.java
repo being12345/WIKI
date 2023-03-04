d032c3ccf4b0c52a4f88db6db2b50322de1829f5
being <89558990+being12345@users.noreply.github.com>
2023-03-04 21:23:22 +0800

    2-3 test a hello interface using test.http

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,10 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @className: TestController
@@ -19,5 +18,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello! Post" + name;
     }
 }
